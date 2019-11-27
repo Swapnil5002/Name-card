9035c05b308a6d2f2e7b4dbf50e9b04cc37708a5
Swapnil5002 <52300853+Swapnil5002@users.noreply.github.com>
2019-11-28 03:04:27 +0530

    UI Changes

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -53,35 +53,35 @@ export default class Form extends Component {
                 <h3 className="title">REGISTRATION FORM</h3>
                     <div className="sub-container">
                         <div className="name-container">
-                            <p>Name:</p>
-                            <input name="name" type="text" placeholder="Name..." value={this.state.name} onChange={this.handleOnchange}/>
+                            <p>Name</p>
+                            <input name="name" type="text"  className="input" placeholder="Name..." value={this.state.name} onChange={this.handleOnchange}/>
                         </div>
                         <div className="email-container">
-                            <p>Email:</p>
-                            <input name="email" type="text" placeholder="Email..." value={this.state.email}/>
+                            <p>Email</p>
+                            <input name="email" type="text" className="input" placeholder="Email..." value={this.state.email}/>
                         </div>
                         <div className="phone-container">
-                            <p>Phone:</p>
+                            <p>Phone</p>
                             <label className="prefix">+91</label>
-                            <input name="phone" type="text" placeholder="" value={this.state.phone}/>
-                            <label><FontAwesomeIcon icon={faPlus} onClick={this.handleaddPhone}/></label>
-                            <label><FontAwesomeIcon icon={faTimes} onClick={this.handledeletePhone}/></label>
-                            {this.state.addPhone ? <p><input name="phone" type="text" placeholder=""/></p> : "" }
+                            <input name="phone" type="text" placeholder="Pone Number" className="input" value={this.state.phone}/>
+                            <label><FontAwesomeIcon icon={faPlus} onClick={this.handleaddPhone} className="icon"/></label>
+                            <label><FontAwesomeIcon icon={faTimes} onClick={this.handledeletePhone} className="icon"/></label>
+                            {this.state.addPhone ? <p><input name="phone" type="text" placeholder="Phone Number"/></p> : "" }
                         </div>
                         <div className="password-container">
-                            <p>Password:</p>
-                            <input type="text" name="password" placeholder="Password..."/>
+                            <p>Password</p>
+                            <input type="text" name="password" className="input" placeholder="Password..."/>
                         </div>
                         <div className="confirmpass-container">
-                            <p>Confirm Password:</p>
-                            <input name="confirm-password" type="text" placeholder="Confirm password..."/>
+                            <p>Confirm Password</p>
+                            <input name="confirm-password" type="text" className="input" placeholder="Confirm password..."/>
                         </div>
                         <div className="checkbox-container">
-                            <input name="checkbox" type="checkbox" />
+                            <input name="checkbox" type="checkbox" className="checkbox" />
                             <label>I agree to the terms and conditions</label>
                         </div>
                         <div className="button-container">
-                            <Button variant="contained" color="primary">Register</Button>
+                            <Button variant="contained" color="primary" className="button">Register</Button>
                         </div>
                     </div>
             </div>
